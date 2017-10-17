da517ed34f9197d0b4004e072e018c195e512e85
dbutchy <dbutchy@vib.com>
2017-10-17 16:05:20 -0400

    added login form

--- index.php
+++ index.php
@@ -1,1 +1,48 @@
-<?php echo "Hello, Dave!"; ?>
+<DOCTYPE html>
+
+	<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE-edge" />
+		<meta name="viewport" content="width=device-width, intial-scale=1" />
+		<meta name="robots" content="follow" />
+		<title>PHP Login Course</title>
+		<base href="/" />
+		<!-- UIkit CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/css/uikit.min.css" />
+
+	</head>
+	<body>
+		<!-- jQuery is required -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+		<!-- UIkit JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit-icons.min.js"></script>
+		<div class="uk-section uk-container">
+			<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+				<form class="uk-form-stacked js-login">
+
+				    <div class="uk-margin">
+				        <label class="uk-form-label" for="form-stacked-text">Email</label>
+				        <div class="uk-form-controls">
+				            <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="joe@gmail.com">
+				        </div>
+				    </div>
+
+				    <div class="uk-margin">
+				        <label class="uk-form-label" for="form-stacked-text">Password</label>
+				        <div class="uk-form-controls">
+				            <input class="uk-input" id="form-stacked-text" type="password"  required="required" placeholder="your password">
+				        </div>
+				    </div>
+
+				    <div class="uk-margin">
+				        <button class="uk-button uk-button-default" type="submit">Log-in</button>
+				    </div>
+				</form>
+			</div>
+			<!-- <?php echo "Hello,    David!  You are looking spritely today."; ?>  -->
+		</div>
+	</body>
+	</html>
